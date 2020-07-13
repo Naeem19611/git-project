fb3b6f80ec6ecb259c815cb381ab361a44072480
naeem <naeem.mizan143@gmail.com>
2020-07-13 19:51:55 +0600

    Added a new method called display to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,14 @@
 public class hello{
     public static void main(string[] args){
         system.outprintln("vs code is awesome..")
+
+        greeting("Naeem");
     }
+
+
+    public static void greeting(string name){
+        system.out.println("welcome " .name);
+    }
+
 }
+
